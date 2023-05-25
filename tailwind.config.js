89b875c6268be62089553731bbcee15e409d6dda
jpdotdev <johndpezza@gmail.com>
2023-05-25 10:19:14 -0400

    visit WV site made with Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+            "./dist/**/*.{html,js}"],
+  theme: { 
+    screens: {
+      'xs': '200px',
+
+      'sm': '620px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+
+      'xl': '1740px'
+      // => @media (min-width: 1440px) { ... }
+    },
+    fontSize: {
+      xxs: '1.5rem',
+      xs: '2rem',
+      sm: '3rem',
+      md: '4rem',
+      lg: '5.5rem',
+      xl: '8rem',
+      xxl: '10rem'
+    },
+    extend: {
+      height: {
+        xs: '17vh',
+        sm: '23vh',
+        md: '32vh',
+        lg: '40vh',
+        xl: '60vh'
+      },
+      fontFamily: {
+        'bebas': ['"Bebas Neue"', 'sans-serif'],
+        'perm': ['"Permanent Marker"', 'cursive'],
+        'darum': ['"Darumadrop One"', 'cursive']
+      },
+    },
+  },
+  plugins: [],
+}
+
